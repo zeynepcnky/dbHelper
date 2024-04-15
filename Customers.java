3b2fd9245076f0d3e7066094f6641ed16b603496
Zeynep Çankaya <161689003+zeynepcnky@users.noreply.github.com>
2024-04-15 11:16:43 +0300

    Add files via upload

---
+++ Customers.java
@@ -0,0 +1,17 @@
+public class Customers {
+
+      private String customer_id;
+      private String first_name;
+      private String last_name;
+      private String birth_date;
+      private String city;
+
+      public Customers(String customer_id, String first_name, String last_name, String birth_date, String city) {
+
+         this.customer_id = customer_id;
+         this.first_name = first_name;
+         this.last_name = last_name;
+         this.birth_date = birth_date;
+         this.city = city;
+    }
+}
